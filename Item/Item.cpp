e654a8cc85d258a2289fb486da7ef68df39f4163
47zzz <glory404058@gmail.com>
2023-12-17 10:04:20 +0800

    新增print item

--- Item/Item.cpp
+++ Item/Item.cpp
@@ -7,11 +7,11 @@ using namespace std;
 
 Item::~Item()
 {
-    cout << "Items' destructor implementation";
+    
 }
 void Item::print()
 {
-    cout << "Player have " << itemNum << " " << itemName << "\nThis item will gain you\n" << this -> increaseRate.iq << " Intellgence\n" << this -> increaseRate.physical <<" Strength\n" << this -> increaseRate.talent << " Talent\n" << this -> increaseRate.wealth << " Wealth\n" << this -> increaseRate.luck << " Luck\n" << this -> increaseRate.appearance << " Appearance\nUsing this item on other characters will increase " << impressionIncre << " impression of you" ;
+    cout << "Player have " << itemNum << " " << itemName << "\nThis item will gain you\n" << this -> increaseRate.iq << " Intellgence\n" << this -> increaseRate.physical <<" Strength\n" << this -> increaseRate.talent << " Talent\n" << this -> increaseRate.wealth << " Wealth\n" << this -> increaseRate.luck << " Luck\n" << this -> increaseRate.appearance << " Appearance\nUsing this item on other characters will increase " << impressionIncre << " impression of you\n" ;
 }
 void Item::gainThisItem()
 {
